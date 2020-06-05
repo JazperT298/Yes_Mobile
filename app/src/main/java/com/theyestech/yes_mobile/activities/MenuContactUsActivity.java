1a64d5e1bff7961c31ee555ba94a7347711095b8
jazper298 <minatosjutsu@gmail.com>
2020-06-05 17:16:53 +0800

    contact us ui and functionality

--- app/src/main/java/com/theyestech/yes_mobile/activities/MenuContactUsActivity.java
+++ app/src/main/java/com/theyestech/yes_mobile/activities/MenuContactUsActivity.java
@@ -1,14 +1,24 @@
 package com.theyestech.yes_mobile.activities;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.theyestech.yes_mobile.R;
 import com.theyestech.yes_mobile.utils.UserRole;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import es.dmoral.toasty.Toasty;
 
 public class MenuContactUsActivity extends AppCompatActivity {
 
@@ -16,7 +26,9 @@ public class MenuContactUsActivity extends AppCompatActivity {
 
     private String role;
 
-    private ImageView iv_ContactBack;
+    private ImageView iv_ContactBack, imageViewFb,imageViewTw,imageVieIns;
+    private Button btn_message;
+    private EditText et_FullName, et_Email, et_Message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +47,13 @@ public class MenuContactUsActivity extends AppCompatActivity {
 
     private void initializeUI(){
         iv_ContactBack = findViewById(R.id.iv_ContactBack);
+        imageViewFb = findViewById(R.id.imageViewFb);
+        imageViewTw = findViewById(R.id.imageViewTw);
+        imageVieIns = findViewById(R.id.imageVieIns);
+        btn_message = findViewById(R.id.btn_message);
+        et_FullName = findViewById(R.id.et_FullName);
+        et_Email = findViewById(R.id.et_Email);
+        et_Message = findViewById(R.id.et_Message);
 
         iv_ContactBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,5 +61,101 @@ public class MenuContactUsActivity extends AppCompatActivity {
                 finish();
             }
         });
+        imageViewFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/Yestech-104889414269548/"));
+                    startActivity(intent);
+                } catch(Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/Yestech-104889414269548/")));
+                }
+            }
+        });
+        imageViewTw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toasty.error(context, "Maintenance").show();
+            }
+        });
+        imageVieIns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/theyestech/"));
+                    startActivity(intent);
+                } catch(Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/theyestech/")));
+                }
+            }
+        });
+
+        btn_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = et_FullName.getText().toString();
+                String email = et_Email.getText().toString();
+                String message = et_Message.getText().toString();
+
+                if (TextUtils.isEmpty(name)){
+                    et_FullName.setError("Enter Your Name");
+                    et_FullName.requestFocus();
+                    return;
+                }
+
+                Boolean onError = false;
+                if (!isValidEmail(email)) {
+                    onError = true;
+                    et_Email.setError("Invalid Email");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(message)){
+                    et_Message.setError("Enter Your Message");
+                    et_Message.requestFocus();
+                    return;
+                }
+
+                Intent sendEmail = new Intent(android.content.Intent.ACTION_SEND);
+
+                /* Fill it with Data */
+                sendEmail.setType("plain/text");
+                sendEmail.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"jaspertony.atillo@gmail.com"});
+                sendEmail.putExtra(android.content.Intent.EXTRA_TEXT,
+                        "name:"+name+'\n'+"Email ID : "+email+'\n'+"Message:"+'\n'+message);
+
+                /* Send it off to the Activity-Chooser */
+                startActivity(Intent.createChooser(sendEmail, "Send mail..."));
+                et_FullName.setText("");
+                et_Email.setText("");
+                et_Message.setText("");
+            }
+        });
+
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //Get a Tracker (should auto-report)
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+
+    // validating email id
+
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
