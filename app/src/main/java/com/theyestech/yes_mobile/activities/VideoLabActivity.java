ac1ba348ab383558544a18fbdf4058d94174a55e
jazper298 <minatosjutsu@gmail.com>
2020-06-06 13:48:58 +0800

    prepaing videolabactivity and videolab model

---
+++ app/src/main/java/com/theyestech/yes_mobile/activities/VideoLabActivity.java
@@ -0,0 +1,17 @@
+package com.theyestech.yes_mobile.activities;
+
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.theyestech.yes_mobile.R;
+
+public class VideoLabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_lab);
+    }
+}
---
+++ app/src/main/java/com/theyestech/yes_mobile/models/VideoLab.java
@@ -0,0 +1,86 @@
+package com.theyestech.yes_mobile.models;
+
+public class VideoLab {
+
+    private String vh_id;
+    private String vh_title;
+    private String educator_fullname;
+    private String video_price;
+    private String video_title;
+    private String video_dateupload;
+    private String video_type;
+    private String video_id;
+    private String video_filename;
+
+    public String getVh_id() {
+        return vh_id;
+    }
+
+    public void setVh_id(String vh_id) {
+        this.vh_id = vh_id;
+    }
+
+    public String getVh_title() {
+        return vh_title;
+    }
+
+    public void setVh_title(String vh_title) {
+        this.vh_title = vh_title;
+    }
+
+    public String getEducator_fullname() {
+        return educator_fullname;
+    }
+
+    public void setEducator_fullname(String educator_fullname) {
+        this.educator_fullname = educator_fullname;
+    }
+
+    public String getVideo_price() {
+        return video_price;
+    }
+
+    public void setVideo_price(String video_price) {
+        this.video_price = video_price;
+    }
+
+    public String getVideo_title() {
+        return video_title;
+    }
+
+    public void setVideo_title(String video_title) {
+        this.video_title = video_title;
+    }
+
+    public String getVideo_dateupload() {
+        return video_dateupload;
+    }
+
+    public void setVideo_dateupload(String video_dateupload) {
+        this.video_dateupload = video_dateupload;
+    }
+
+    public String getVideo_type() {
+        return video_type;
+    }
+
+    public void setVideo_type(String video_type) {
+        this.video_type = video_type;
+    }
+
+    public String getVideo_id() {
+        return video_id;
+    }
+
+    public void setVideo_id(String video_id) {
+        this.video_id = video_id;
+    }
+
+    public String getVideo_filename() {
+        return video_filename;
+    }
+
+    public void setVideo_filename(String video_filename) {
+        this.video_filename = video_filename;
+    }
+}
